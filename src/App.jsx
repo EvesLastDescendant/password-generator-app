4b6f3ee427284cd9b4730a069f97a5248814a95b
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2023-07-08 13:52:09 +0100

    updated jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faKeycdn } from "@fortawesome/free-brands-svg-icons"
+import { faClipboard, faGear, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      value: 8,
+      checked: false,
+      generatedPassword: "Click the Generate button",
+      copied: false
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    this.rangeOnInput = this.rangeOnInput.bind(this)
+    this.boxChecked = this.boxChecked.bind(this)
+    this.generate = this.generate.bind(this)
+    this.copyPassword = this.copyPassword.bind(this)
+  }
+
+  rangeOnInput = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+  boxChecked = (event) => {
+    this.setState({checked: event.target.checked})
+  }
+
+  generate = () => {
+    let password =  ""
+    const chars =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    const specialChars = '!@#$%^&*()_-+[]{}|/' 
+    
+    
+    for(let i = 0; i < this.state.value; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length)
+      password += chars.charAt(randomIndex)
+    }
+
+    if(this.state.checked) {
+      const randomSpecialIndex = Math.floor(Math.random() * specialChars.length)
+      password = password.slice(0, randomSpecialIndex) + specialChars.charAt(randomSpecialIndex) + password.slice(randomSpecialIndex + 1)
+    }
+    this.setState({count: this.state.count + 1})
+    this.setState({generatedPassword: password})
+    this.setState({copied: false})
+  }
+
+  copyPassword = async () => {
+    try {
+      await navigator.clipboard.writeText(this.state.generatedPassword)
+      this.setState({copied: true})
+    } catch (err) {
+      this.setState({copied: false})
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="generator-box">
+          <div className="card">
+            <button 
+              className='count-btn'
+              onClick={this.counter}>Generated {this.state.count} {this.state.count === 0 || this.state.count === 1 ? "time" : "times"}
+            </button><FontAwesomeIcon icon={faGear} spin/>
+          </div>
+          <div className="container">
+            <h3>Password Generator</h3>
+            <div className="slider">
+              <input 
+                type="range"
+                value={this.state.value}
+                min="1"
+                max="50"
+                onChange={this.rangeOnInput}
+              />
+              <output>{this.state.value}</output>
+            </div>
+            <button className='generate-btn' onClick={this.generate}>
+              Generate <FontAwesomeIcon icon={faKeycdn}/>
+            </button>
+            <div className="checkbox">
+              <label htmlFor="checkbox">Include special characters</label>
+              <input 
+                type="checkbox"
+                checked={this.state.checked}
+                id='checkbox'
+                onChange={this.boxChecked} 
+              />
+            </div>
+          </div>
+          <div className="output">
+            <h5><span>
+              {this.state.generatedPassword}
+              <FontAwesomeIcon icon={faArrowRight} />
+            </span></h5>
+            <button className='clipboard' disabled={!this.state.generatedPassword} onClick={this.copyPassword}>
+              <FontAwesomeIcon icon={faClipboard}/>
+            </button>
+          </div>
+          <div className="copy">
+            {this.state.copied ? 
+              this.state.count !== 0 ?
+              (
+                <span className='span-copy'>
+                  password copied
+                </span>
+              ) : 
+              (
+                <span className='span-notcopy'>
+                  password not generated
+                </span>
+              ) :
+              ""
+            }
+          </div>
+        </div>
       </div>
-      <h1>Vite + React Sup!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
 }
 
 export default App
